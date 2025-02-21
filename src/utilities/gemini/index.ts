3ac4d4e53e81e1a5e87e6a50562f5b4d8685cdbc
Diego González Cruz <diego@thinkey.cl>
2025-02-21 17:24:54 -0500

    gemini fixed

--- src/utilities/gemini/index.ts
+++ src/utilities/gemini/index.ts
@@ -7,3 +7,19 @@ export const generateGrade = async (prompt: string) => {
   const result = await model.generateContent(prompt)
   return result.response.text()
 }
+
+export const generateSummary = async (data: any) => {
+  //   console.log('generate Summary Fn starting')
+  const prompt = `
+    You are a friendly and empathetic assistant for a teacher, here to provide a clear glance at the status of their Google Classroom courses and assignments.
+    Using the provided JSON data, craft a concise summary that covers key statistics like student engagement, course activity, and assignment progress.
+    Emphasize a supportive tone that reassures the teacher, highlights areas of success, and offers insights in a positive and encouraging manner.
+    Remember: you're more than a grading tool—your insights help the teacher understand the classroom dynamics and motivate them to keep doing great work.
+  `
+
+  //   console.log(prompt, 'PROMPT')
+
+  const result = await model.generateContent([prompt, JSON.stringify(data)])
+  //   console.log(result.response.text(), 'RESULT')
+  return result.response.text()
+}
