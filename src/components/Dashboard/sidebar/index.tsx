ff6b787a52a8dd2c58513116dcb4fa93642e08b8
Diego González Cruz <diego@thinkey.cl>
2025-03-05 00:04:13 -0500

    fixed sidebar logout

--- src/components/Dashboard/sidebar/index.tsx
+++ src/components/Dashboard/sidebar/index.tsx
@@ -55,7 +55,7 @@ export function Sidebar({ className }: React.HTMLAttributes<HTMLDivElement>) {
 
   const handleLogout = async () => {
     console.log('logging out')
-    const res = await fetch('http://localhost:3000/api/users/logout', {
+    const res = await fetch('/api/users/logout', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
