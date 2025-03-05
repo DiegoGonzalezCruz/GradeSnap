dc2ce6baad2f687619fbf6dd54327300220ac6a8
Diego González Cruz <diego@thinkey.cl>
2025-03-04 22:40:32 -0500

    hero component with login

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -4,10 +4,11 @@ import React, { useEffect } from 'react'
 
 import type { Page } from '@/payload-types'
 
-import { CMSLink } from '@/components/Link'
-import { Media } from '@/components/Media'
-import RichText from '@/components/RichText'
-import ImageComponent from '@/components/Media/ImageComponent'
+// import { CMSLink } from '@/components/Link'
+// import { Media } from '@/components/Media'
+// import RichText from '@/components/RichText'
+// import ImageComponent from '@/components/Media/ImageComponent'
+import LoginComponent from '@/app/(frontend)/(login)/login/LoginComponent'
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
@@ -16,30 +17,5 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
     setHeaderTheme('dark')
   })
 
-  return (
-    <div
-      className="h-[70vh]  flex flex-row items-center justify-center  w-5/6 mx-auto"
-      data-theme="dark"
-    >
-      <div className=" flex flex-row h-full w-full">
-        <div className=" w-1/2 h-full">
-          {media && <ImageComponent image={media} alt="" priority className="rounded-3xl" />}
-        </div>
-        <div className="w-1/2  flex flex-col items-center justify-center">
-          {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
-          {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
-              {links.map(({ link }, i) => {
-                return (
-                  <li key={i}>
-                    <CMSLink {...link} />
-                  </li>
-                )
-              })}
-            </ul>
-          )}
-        </div>
-      </div>
-    </div>
-  )
+  return <LoginComponent />
 }
